aeff1626c0f2c3572d3558899339ba7098a3f90a
Ryan Kloss <102738640+RyanKl22@users.noreply.github.com>
2024-05-21 20:04:08 -0300

    Add files via upload

---
+++ Experiência Criativa - Projeto/salvar_roteiro.php
@@ -0,0 +1,72 @@
+<?php
+
+include 'auth.php';
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "JBB";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Conexão falhou: " . $conn->connect_error);
+}
+
+$response = ['success' => false];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $titulo = $_POST['titulo'];
+    $descricao = $_POST['descricao'];
+    $privacidade = isset($_POST['privacidade']) ? 1 : 0;
+    $localizacoes = $_POST['localizacao'];
+    $valores = $_POST['valor'];
+    $moeda = $_POST['moeda'];
+    $imagens = $_FILES['imagens'];
+
+    $id_usuario = $_SESSION['usuario_id'];
+
+    $sql_roteiro = "INSERT INTO roteirosviagem (id_usuario, titulo, descricao, publico_privado) 
+                    VALUES ('$id_usuario', '$titulo', '$descricao', '$privacidade')";
+    mysqli_query($conn, $sql_roteiro);
+
+    $id_roteiro = mysqli_insert_id($conn);
+
+    for ($i = 0; $i < count($localizacoes); $i++) {
+        $localizacao = $localizacoes[$i];
+        $valor = $valores[$i];
+        $latitude = $_POST['latitude'][$i];
+        $longitude = $_POST['longitude'][$i];
+
+        $sql_localizacao = "INSERT INTO localizacoesvalores (id_roteiro, nome_local, latitude, longitude, valor_gasto, moeda) 
+                            VALUES ('$id_roteiro', '$localizacao', '$latitude', '$longitude', '$valor', '$moeda')";
+        mysqli_query($conn, $sql_localizacao);
+    }
+
+    if (!empty($imagens['name'][0])) {
+        $target_dir = "uploads/";
+
+        if (!is_dir($target_dir)) {
+            mkdir($target_dir, 0777, true);
+        }
+
+        for ($i = 0; $i < count($imagens['name']); $i++) {
+            $target_file = $target_dir . basename($imagens['name'][$i]);
+            $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+            $check = getimagesize($imagens['tmp_name'][$i]);
+            if ($check !== false) {
+                if (move_uploaded_file($imagens['tmp_name'][$i], $target_file)) {
+                    $sql_imagem = "INSERT INTO imagens (id_roteiro, imagem) VALUES ('$id_roteiro', '$target_file')";
+                    mysqli_query($conn, $sql_imagem);
+                }
+            }
+        }
+    }
+
+    $response['success'] = true;
+}
+
+echo json_encode($response);
+?>
---
+++ Experiência Criativa - Projeto/upload_imagem.php
@@ -0,0 +1,42 @@
+<?php
+
+include 'auth.php';
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "JBB";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Conexão falhou: " . $conn->connect_error);
+}
+
+$response = ['success' => false, 'filepaths' => []];
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES['imagens'])) {
+
+    $target_dir = "uploads/";
+
+    if (!is_dir($target_dir)) {
+        mkdir($target_dir, 0777, true);
+    }
+
+    for ($i = 0; $i < count($_FILES['imagens']['name']); $i++) {
+        $target_file = $target_dir . basename($_FILES['imagens']['name'][$i]);
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+        $check = getimagesize($_FILES['imagens']['tmp_name'][$i]);
+        if ($check !== false) {
+            if (move_uploaded_file($_FILES['imagens']['tmp_name'][$i], $target_file)) {
+                $response['filepaths'][] = $target_file;
+            }
+        }
+    }
+
+    $response['success'] = true;
+}
+
+echo json_encode($response);
+?>
